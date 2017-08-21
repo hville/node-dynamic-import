29f73d4410fffa663627894f1a6ea48b60643a83
hville <hugov@runbox.com>
2017-08-21 13:18:43 -0400

    rollup 0.48.2 and options.input

--- es-import.js
+++ es-import.js
@@ -1,17 +1,11 @@
-//node-es-module-loader 3044 files, 1079 folders, 5.37MB
-//node-dynamic-import     49 files,    9 folders, 3.69MB
+const rollup = require( 'rollup' ),
+			path = require('path')
 
-var rollup = require( 'rollup' ),
-		nodePath = require('path')
-
-module.exports = function importModule(path) {
-	return rollup.rollup({
-		entry: nodePath.resolve(nodePath.dirname(module.parent.filename), path)
-	}).then(getCode)
-}
-
-function getCode(bundle) {
-	return bundle.generate({format: 'cjs'}).then(getExports)
+module.exports = function importModule(file) {
+	const input = path.resolve(path.dirname(module.parent.filename), file)
+	return rollup.rollup({input})
+		.then(bundle => bundle.generate({format: 'cjs'}))
+		.then(getExports)
 }
 
 function getExports(codeMap) {
